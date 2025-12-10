e48871ef546b3b58d1f145c6d8be54415cffdc1d
Jim Ma <ema@redhat.com>
2025-12-10 13:31:14 +0800

    Add hypershift test

--- test/extend/go.mod
+++ test/extend/go.mod
@@ -3,34 +3,15 @@ module github.com/openshift/hypershift/test/extend
 go 1.24.4
 
 require (
-	cel.dev/expr v0.24.0
-	github.com/antlr4-go/antlr/v4 v4.13.0
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/blang/semver/v4 v4.0.0
-	github.com/go-logr/logr v1.4.3
-	github.com/go-task/slim-sprig/v3 v3.0.0
-	github.com/google/cel-go v0.26.0
-	github.com/google/go-cmp v0.7.0
-	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6
-	github.com/inconshreveable/mousetrap v1.1.0
 	github.com/onsi/ginkgo/v2 v2.27.2
 	github.com/onsi/gomega v1.38.2
 	github.com/openshift-eng/openshift-tests-extension v0.0.0-20251113163031-356b66aa5c24
 	github.com/openshift/origin v1.5.0-alpha.3.0.20251203005656-6264449a6c8b
-	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.9.1
-	github.com/spf13/pflag v1.0.7
-	github.com/stoewer/go-strcase v1.3.0
 	github.com/tidwall/gjson v1.18.0
 	go.etcd.io/etcd/client/v3 v3.6.4
-	go.yaml.in/yaml/v3 v3.0.4
-	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c
-	golang.org/x/net v0.43.0
-	golang.org/x/sys v0.36.0
-	golang.org/x/text v0.29.0
-	golang.org/x/tools v0.36.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20250818200422-3122310a409c
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250826171959-ef028d996bc1
-	google.golang.org/protobuf v1.36.8
 	k8s.io/api v0.34.2
 	k8s.io/apimachinery v0.34.2
 	k8s.io/apiserver v0.34.2
@@ -40,6 +21,7 @@ require (
 )
 
 require (
+	cel.dev/expr v0.24.0 // indirect
 	cloud.google.com/go v0.121.6 // indirect
 	cloud.google.com/go/auth v0.16.5 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
@@ -83,6 +65,7 @@ require (
 	github.com/Microsoft/hnslib v0.1.1 // indirect
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/ProtonMail/go-crypto v1.1.6 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/armon/circbuf v0.0.0-20190214190532-5111143e8da2 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
@@ -125,6 +108,7 @@ require (
 	github.com/ghodss/yaml v1.0.1-0.20190212211648-25d852aebe32 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/analysis v0.23.0 // indirect
 	github.com/go-openapi/errors v0.22.1 // indirect
@@ -139,6 +123,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -147,8 +132,11 @@ require (
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/cadvisor v0.52.1 // indirect
+	github.com/google/cel-go v0.26.0 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
@@ -166,6 +154,7 @@ require (
 	github.com/hashicorp/hc-install v0.9.2 // indirect
 	github.com/hashicorp/terraform-exec v0.23.0 // indirect
 	github.com/hashicorp/terraform-json v0.25.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -219,6 +208,7 @@ require (
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.23.0 // indirect
@@ -228,8 +218,10 @@ require (
 	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.7 // indirect
 	github.com/spiffe/go-spiffe/v2 v2.5.0 // indirect
 	github.com/std-uritemplate/std-uritemplate/go/v2 v2.0.3 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.11.0 // indirect
 	github.com/tecbiz-ch/nutanix-go-sdk v0.1.15 // indirect
@@ -260,15 +252,24 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/term v0.35.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 	google.golang.org/api v0.247.0 // indirect
 	google.golang.org/genproto v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250818200422-3122310a409c // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250826171959-ef028d996bc1 // indirect
 	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	gopkg.in/go-jose/go-jose.v2 v2.6.3 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
--- test/extend/hypershift.go
+++ test/extend/hypershift.go
@@ -2,38 +2,34 @@ package extend
 
 import (
 	"context"
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/blang/semver/v4"
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
-
 	exutil "github.com/openshift/origin/test/extended/util"
+
 	compat_otp "github.com/openshift/origin/test/extended/util/compat_otp"
-	"github.com/openshift/origin/test/extended/util/compat_otp/clusterinfra"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
 var _ = g.Describe("[sig-hypershift] Hypershift", func() {
-	defer g.GinkgoRecover()
+	//defer g.GinkgoRecover()
 
 	var (
-		oc                    = exutil.NewCLIWithoutNamespace("hypershift")
-		iaasPlatform          string
+		oc *exutil.CLI
+		//iaasPlatform          string
 		hostedcluster         *hostedCluster
 		hostedclusterPlatform PlatformType
 	)
-
-	if err := compat_otp.InitTest(false); err != nil {
+	if err := compat_otp.InitTest(true); err != nil {
 		panic(err)
 	}
 	e2e.AfterReadingAllFlags(compat_otp.TestContext)
 	g.BeforeEach(func(ctx context.Context) {
+		oc = exutil.NewCLIForMonitorTest("hypershift")
+		//This is a workaround to set the testStarted=true, then it can allow invoking compat_otp.ValidHypershiftAndGetGuestKubeConf(oc)
 		exutil.WithCleanup(func() {
-			fmt.Println("cleanup after each test")
+			e2e.Logf("This is called by the workaround to set the test to be started")
 		})
+
 		hostedClusterName, hostedclusterKubeconfig, hostedClusterNs := compat_otp.ValidHypershiftAndGetGuestKubeConf(oc)
 		oc.SetGuestKubeconf(hostedclusterKubeconfig)
 		hostedcluster = newHostedCluster(oc, hostedClusterNs, hostedClusterName)
@@ -45,7 +41,7 @@ var _ = g.Describe("[sig-hypershift] Hypershift", func() {
 		}
 
 		// get IaaS platform
-		iaasPlatform = compat_otp.ExtendedCheckPlatform(ctx, oc)
+		//iaasPlatform = compat_otp.ExtendedCheckPlatform(ctx, oc)
 		//hypershiftTeamBaseDir = compat_otp.FixturePath("testdata", "hypershift")
 		// hosted cluster infra ID
 		//hcInfraID = doOcpReq(oc, OcpGet, true, "hc", hostedClusterName, "-n", hostedClusterNs, `-ojsonpath={.spec.infraID}`)
@@ -58,152 +54,32 @@ var _ = g.Describe("[sig-hypershift] Hypershift", func() {
 		}
 
 	})
-
-	// author: ema@redhat.com
-	g.It("Author:ema-HyperShiftMGMT-Critical-83131-Setting AWS tags on existing Hosted Clusters and their Nodepools happens without roll out", func() {
-		if iaasPlatform != "aws" {
-			g.Skip("Day2 tag test on HostedCluster and NodePool is only on AWS and skip this test on:" + iaasPlatform)
-		}
-		hcVersion := compat_otp.GetHostedClusterVersion(oc, hostedcluster.name, hostedcluster.namespace)
-		e2e.Logf("Found hosted cluster version = %q", hcVersion)
-		hcVersion.Pre = nil
-		minHcVersion := semver.MustParse("4.19.0")
-		if hcVersion.LT(minHcVersion) {
-			g.Skip(fmt.Sprintf("Skip the test on HostedCluster version lower than 4.19. The current HostedCluster version: %s", hcVersion))
-		}
+	// author: heli@redhat.com
+	g.It("ROSA-OSD_CCS-HyperShiftMGMT-Author:heli-Critical-42855-Check Status Conditions for HostedControlPlane", func() {
 		rc := hostedcluster.checkHCConditions()
 		o.Expect(rc).Should(o.BeTrue())
 
+		// add more test here to check hypershift util
 		operatorNS := compat_otp.GetHyperShiftOperatorNameSpace(oc)
 		e2e.Logf("hosted cluster operator namespace %s", operatorNS)
 		o.Expect(operatorNS).NotTo(o.BeEmpty())
 
-		//get guest cluster worker node creation time
-		guestName, guestKube, _ := compat_otp.ValidHypershiftAndGetGuestKubeConfWithNoSkip(oc)
-		o.Expect(guestName).NotTo(o.BeEmpty())
-		o.Expect(guestKube).NotTo(o.BeEmpty())
+		hostedclusterNS := compat_otp.GetHyperShiftHostedClusterNameSpace(oc)
+		e2e.Logf("hosted cluster namespace %s", hostedclusterNS)
+		o.Expect(hostedclusterNS).NotTo(o.BeEmpty())
 
-		resourceTagPatch := `[
-			              {
-			                "op": "add",
-			                "path": "/spec/platform/aws/resourceTags/0",
-			                "value": {
-			                  "key": "hypershift.io/hostedcluster/qe-test",
-			                  "value": "owned"
-			                 }
-			              }
-			         ]`
-		e2e.Logf("Add resource tag: %s", resourceTagPatch)
-
-		doOcpReq(oc, OcpPatch, true, "hostedcluster", hostedcluster.name, "-n", hostedcluster.namespace, "--type=json", "-p="+resourceTagPatch)
-
-		//revert the resource tag change
-		resourceTagRevertPatch := `[
-			              {
-			                "op": "remove",
-			                "path": "/spec/platform/aws/resourceTags/0"
-			              }
-			         ]`
-		defer doOcpReq(oc, OcpPatch, true, "hostedcluster", hostedcluster.name, "-n", hostedcluster.namespace, "--type=json", "-p="+resourceTagRevertPatch)
-
-		//get the first nodepool
-		nodePool := doOcpReq(oc, OcpGet, true, "nodepool", "-n", hostedcluster.namespace, "-ojsonpath={.items[0].metadata.name}")
-		e2e.Logf("Get the node pool name: %s", nodePool)
-
-		//check if the hostedcluster tag is added to aws instance
-		awsInstanceIDs := hostedcluster.getAWSInstanceIDs(nodePool)
-		//get the youngest age aws instance
-		awsInstanceID := awsInstanceIDs[len(awsInstanceIDs)-1]
-		parts := strings.Split(awsInstanceID, "/")
-		awsInstance := parts[len(parts)-1]
-		//check aws tags
-		clusterinfra.GetAwsCredentialFromCluster(oc)
-		region := doOcpReq(oc, OcpGet, true, "hostedcluster", hostedcluster.name, "-n", hostedcluster.namespace, "-ojsonpath={.spec.platform.aws.region}")
-		e2e.Logf("The aws region of the hosted cluster is %s", region)
-		awsClient := compat_otp.InitAwsSessionWithRegion(region)
-		instanceTags, err := awsClient.DescribeTags("instance", awsInstance)
+		guestClusterName, guestClusterKube, _ := compat_otp.ValidHypershiftAndGetGuestKubeConf(oc)
+		e2e.Logf("hostedclustercluster name %s", guestClusterName)
+		cv, err := oc.AsAdmin().SetGuestKubeconf(guestClusterKube).AsGuestKubeconf().Run("get").Args("clusterversion").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
-		awsInstanceTags := instanceTags.String()
-		e2e.Logf("Get aws insance: %s tags : %s", awsInstanceID, awsInstanceTags)
-		o.Expect(strings.Contains(awsInstanceTags, "hypershift.io/hostedcluster/qe-test")).To(o.BeTrue())
+		e2e.Logf("hosted cluster clusterversion name %s", cv)
 
-		//Check security group tag
-		securityGroups, err := awsClient.GetInstanceSecurityGroupIDs(awsInstance)
+		guestClusterName, guestClusterKube, _ = compat_otp.ValidHypershiftAndGetGuestKubeConfWithNoSkip(oc)
+		o.Expect(guestClusterName).NotTo(o.BeEmpty())
+		o.Expect(guestClusterKube).NotTo(o.BeEmpty())
+		cv, err = oc.AsAdmin().SetGuestKubeconf(guestClusterKube).AsGuestKubeconf().Run("get").Args("clusterversion").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
-		if len(securityGroups) != 0 {
-			exists := false
-			for _, securityGroup := range securityGroups {
-				securityGroupTags, err := awsClient.DescribeTags("security-group", securityGroup)
-				o.Expect(err).NotTo(o.HaveOccurred())
-				securityGroupStringTags := securityGroupTags.String()
-				e2e.Logf("Get security group tags : %s", securityGroupStringTags)
-				if strings.Contains(securityGroupStringTags, "hypershift.io/hostedcluster/qe-test") {
-					exists = true
-					break
-				}
-			}
-			o.Expect(exists).To(o.BeTrue(), "expected the security group tag 'hypershift.io/hostedcluster/qe-test' to exist but it does not")
-		}
-		//check if the node will be rolled out after HC is tagged
-		o.Consistently(hostedcluster.pollCheckHostedClustersNodePoolReady(nodePool), time.Minute*6, time.Second*10).Should(o.BeTrue())
-		nodePoolTagPatch := `{
-			                  "spec": {
-			                     "platform": {
-				                    "aws": {
- 										"resourceTags": [
-                                           {
-                                            "key": "hypershift.io/nodepool/qe-test",
-                                            "value": "owned"
-                                           }
-		                                ]
-		                            }
-		                         }
-		                      }
-		                    }`
+		e2e.Logf("hosted cluster clusterversion with noskip api name %s", cv)
 
-		nodePoolReplics := hostedcluster.getNodepoolReadyReplicas(nodePool)
-		doOcpReq(oc, OcpPatch, true, "nodepool", nodePool, "-n", hostedcluster.namespace, "--type=merge", "-p="+nodePoolTagPatch)
-
-		nodePoolTagRevertPatch := `[
-			              {
-			                "op": "remove",
-			                "path": "/spec/platform/aws/resourceTags/0"
-			              }
-			         ]`
-		defer doOcpReq(oc, OcpPatch, true, "nodepool", nodePool, "-n", hostedcluster.namespace, "--type=json", "-p="+nodePoolTagRevertPatch)
-
-		//The existing node should not be replaced/recreated
-		o.Consistently(hostedcluster.pollCheckHostedClustersNodePoolReady(nodePool), time.Minute*6, time.Second*10).Should(o.BeTrue())
-
-		//check if this tag is added aws instance
-		instanceTags, err = awsClient.DescribeTags("instance", awsInstance)
-		o.Expect(err).NotTo(o.HaveOccurred())
-		awsInstanceTags = instanceTags.String()
-		e2e.Logf("Get aws insance: %s tags : %s", awsInstanceID, awsInstanceTags)
-		o.Expect(strings.Contains(awsInstanceTags, "hypershift.io/nodepool/qe-test")).To(o.BeTrue())
-
-		//increase nodepool replicas
-		nodePoolReplicaPatch := fmt.Sprintf("{\"spec\":{\"replicas\": %d}}", nodePoolReplics+1)
-		doOcpReq(oc, OcpPatch, true, "nodepool", nodePool, "-n", hostedcluster.namespace, "--type=merge", "-p="+nodePoolReplicaPatch)
-
-		revertNodePoolReplicaPatch := fmt.Sprintf("{\"spec\":{\"replicas\": %d}}", nodePoolReplics)
-		defer doOcpReq(oc, OcpPatch, true, "nodepool", nodePool, "-n", hostedcluster.namespace, "--type=merge", "-p="+revertNodePoolReplicaPatch)
-
-		o.Eventually(hostedcluster.pollCheckHostedClustersNodePoolReady(nodePool), time.Minute*10, time.Second*10).Should(o.BeTrue())
-
-		awsInstanceIDs = hostedcluster.getAWSInstanceIDs(nodePool)
-		//get the youngest age aws instance
-		awsInstanceID = awsInstanceIDs[len(awsInstanceIDs)-1]
-		parts = strings.Split(awsInstanceID, "/")
-		awsInstance = parts[len(parts)-1]
-		e2e.Logf("Get aws insance: %s tags", awsInstance)
-		//check aws tags
-		clusterinfra.GetAwsCredentialFromCluster(oc)
-		instanceTags, err = awsClient.DescribeTags("instance", awsInstance)
-		o.Expect(err).NotTo(o.HaveOccurred())
-		awsInstanceTags = instanceTags.String()
-		e2e.Logf("Get aws insance: tags : %s", awsInstanceTags)
-		o.Expect(strings.Contains(awsInstanceTags, "hypershift.io/nodepool/qe-test")).To(o.BeTrue())
-		o.Expect(strings.Contains(awsInstanceTags, "hypershift.io/hostedcluster/qe-test")).To(o.BeTrue())
 	})
 })
